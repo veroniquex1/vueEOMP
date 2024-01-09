7eb09b8aba6df3b4762e2b681e823e7cc00fccd1
veroniquex1 <vdrossouw@gmail.com>
2024-01-09 09:41:46 +0200

    initial json data

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,47 @@
 import { createStore } from 'vuex'
+const dataURL= 'https://veroniquex1.github.io/vueJSONData/data/'
+
 
 export default createStore({
   state: {
+    // define variables in state
+    // this is taken from the json data
+    jobTitle: null,
+    about: null,
+    education: null,
+    skills: null,
+    testimonials: null,
+    projects: null
   },
   getters: {
   },
   mutations: {
+    setJobTitle(state, value) {
+      state.jobTitle = value;
+  },
+    setAbout(state, value) {
+      state.about = value;
+  },
+    setEducation(state, value) {
+      state.education = value;
+  },
+    setSkills(state, value) {
+      state.skills = value;
+  },
+    setTestimonials(state, value) {
+      state.testimonials = value;
+  },
+    setProjects(state, value) {
+      state.projects = value;
   },
   actions: {
+    async fetchJobTitle(context){
+      let res = await fetch(dataURL) 
+// function has async which is why you use async
+
+      let{jobTitle} = await res.json()
+      context.commit('setJobTitle', jobTitle)
+    }
   },
   modules: {
   }
