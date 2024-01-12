d193258415a3f380239ee9c0d19b0e46ba6ad863
veroniquex1 <vdrossouw@gmail.com>
2024-01-12 14:17:49 +0200

    testimonials added

--- src/views/TestimonialView.vue
+++ src/views/TestimonialView.vue
@@ -1,19 +1,92 @@
 <template>
-    <div class="container">
+    <div class="container" id="testimonialPage">
         <div class="row">
-            <h2 class="display-2">Testimonial</h2>
+            <h1 class="display-2">Testimonials</h1>
         </div>
-        <!-- <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-             </div>
-        </div> -->
+        <div class="card mb-3" style="max-width: 1500px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://i.ibb.co/2nBrV3n/6w9a1127-720.jpg" class="img-fluid ps-5" style="width: 13rem;" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Carl James</h5>
+        <p class="card-text">"I've had the pleasure of working closely with Veronique, and let me tell you, she's an absolute gem in the web development world. I've been consistently amazed by her attention to detail and how efficiently she manages her time. She's a real workhorse, and I mean that in the best way possible.
+But it's not just her technical skills that shine. Veronique's communication and leadership abilities are top-notch. She's always ready to lend a hand to her colleagues, and she's got this knack for quick thinking that's truly impressive. Plus, she's not just a pretty face; she's incredibly sharp.
+If you're looking for a web developer who goes above and beyond, Veronique is your person. I can't recommend her enough for any web development role."</p>
+      </div>
+    </div>
+  </div>
+</div>
+        <div class="card mb-3" style="max-width: 1500px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://i.ibb.co/1XgVV5x/img-1409-720.jpg" class="img-fluid ps-5" style="width: 13rem;" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Thimna Alam</h5>
+        <p class="card-text">Veronique is the most grounded, well mannered and focused person I have ever met. She is one to fixate on a problem until it is perfectly done in other words she leaves no room for error. One skill that mostly possesses is self discipline and at that she also knows how to lead others. If there is one thing I admire about her is that if you go ask her for help if she doesn't know she will tell you but to your surprise she will make it her mission to go do research and come back to you with a solution. Among all of those amazing characteristics she is a respectful person who sees value in each person and everything.</p>
+      </div>
+    </div>
+  </div>
+</div>
+        <div class="card mb-3" style="max-width: 1500px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://i.ibb.co/D7C0dgL/joel2-720.jpg" class="img-fluid ps-5" style="width: 13rem;" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Joel Mukhanya (lecturer)</h5>
+        <p class="card-text">"Veronique is a creative, talented, and well-behaved individual who always prioritizes her work. She excels in collaborating with others and has gained proficiency in HTML5, CSS, and Bootstrap. Including Git and GitHub."</p>
+      </div>
+    </div>
+  </div>
+</div>
+        <div class="card mb-3" style="max-width: 1500px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://i.ibb.co/wr5vxk2/img-20220503-wa0117-109-720.jpg" class="img-fluid ps-5" style="width: 13rem;" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Luqmaan Toffey</h5>
+        <p class="card-text">"When I met Veronique, immediately it became apparent that she has a history with computers. In the short span of 3 days, she unknowingly took me to heights I never knew existed. However, even though she's quite competent, she never misses an opportunity to better herself. Truly one of the most intellectual and humble females you will ever meet. What a privilege it has been to work along side her."</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3" style="max-width: 1500px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://i.ibb.co/dpszYvB/img-8454-480.jpg" class="img-fluid rounded-start" style="width: 13rem;" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Vuyiseka Njovane</h5>
+        <p class="card-text">I have had the pleasure of working closely with Veronique the past few months, and I am continually impressed by her dedication, skills, and passion for software development. As an aspiring developer, one of her standout qualities is her ability to quickly grasp complex technical concepts and turn them into practical solutions. She has a keen eye for detail, always striving for excellence in her work. I've seen her tackle challenging projects with determination, consistently delivering high-quality code and innovative solutions.
+She is not only a skilled aspiring programmer but also an excellent collaborator. She communicate effectively, ask insightful questions, and actively participate in discussions, contributing fresh ideas and problem-solving skills. Her willingness to learn and adapt in the fast-paced world of development is truly commendable.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- <div class="row g-0">
+  <div class="col-md-4">
+    <img src="https://i.ibb.co/1XgVV5x/img-1409-720.jpg" class="img-fluid rounded-start" style="width: 13rem;" alt="">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">Thimna Alam</h5>
+      <p class="card-text"></p>
+    </div>
+  </div>
+</div> -->
+     
 
-        <div>
+        <!-- <div>
             <h3 v-for="info in $store.state.testimonials" :key="info" > {{ info.name }}</h3>
-        </div>
+        </div> -->
     
 </div>
 </template>
@@ -33,5 +106,19 @@
 </script>
 
 <style scoped>
+@import url('https://fonts.cdnfonts.com/css/star-jedi');
+@import url('https://fonts.cdnfonts.com/css/star-wars');
+
+#testimonialPage{
+    width: 100%;
+    height: 100vh;
+}
+
+h1, h5{
+    font-family: 'Star Wars', sans-serif;
+    text-decoration: underline ;
+    text-align: center;
+
+}
 
 </style>
