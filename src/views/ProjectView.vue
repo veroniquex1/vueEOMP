afb179852d7d886e0b6c13a48f3526454e7b9a4c
veroniquex1 <vdrossouw@gmail.com>
2024-01-12 12:29:31 +0200

    project details added

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,8 +1,70 @@
 <template>
-    <div class="container">
+    <div class="container" id="projectPage">
         <div class="row">
             <h2 class="display-2">Projects</h2>
         </div>
+        <div class="card mb-3">
+            <div class="row">
+  <div class="col-sm-6 mb-3 mb-sm-0 mt-3">
+    <div class="card w-75 mb-3">
+  <div class="card-body">
+    <h5 class="card-title">To-Do List</h5>
+    <img src="https://i.ibb.co/3m5Xsfv/printable-to-do-list-template-1-fefefe-010101.png" alt="">
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Github</a>
+    <a href="#" class="btn btn-primary">Netlify</a>
+  </div>
+</div>
+    <div class="card w-75 mb-3">
+  <div class="card-body">
+    <h5 class="card-title">Calculator</h5>
+    <img src="https://i.ibb.co/X5cgG4c/257791.png" alt="">
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Github</a>
+    <a href="#" class="btn btn-primary">Netlify</a>
+  </div>
+</div>
+    <div class="card w-75 mb-3">
+  <div class="card-body">
+    <h5 class="card-title">Bootstrap Portfolio</h5>
+    <img src="https://i.ibb.co/9wb72wX/bootstrap.png" alt="">
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Github</a>
+    <a href="#" class="btn btn-primary">Netlify</a>
+  </div>
+</div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card w-75 mb-3 mt-3">
+  <div class="card-body">
+    <h5 class="card-title">The Book Corner</h5>
+    <img src="https://i.ibb.co/mzPBnS0/57142cdf38e2605105831275646d36a8.jpg" alt="">
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Github</a>
+    <a href="#" class="btn btn-primary">Netlify</a>
+  </div>
+</div>
+    <div class="card w-75 mb-3">
+  <div class="card-body">
+    <h5 class="card-title">Netflix Clone</h5>
+    <img src="https://i.ibb.co/8D1vdpn/netflix.png" alt="netflix">
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Github</a>
+    <a href="#" class="btn btn-primary">Netlify</a>
+  </div>
+</div>
+    <div class="card w-75 mb-3">
+  <div class="card-body">
+    <h5 class="card-title">Something Else</h5>
+    <img src="" alt="">
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Github</a>
+    <a href="#" class="btn btn-primary">Netlify</a>
+  </div>
+</div>
+  </div>
+</div>
+</div>
 
     </div>
 </template>
@@ -14,5 +76,23 @@
 </script>
 
 <style scoped>
+
+#projectPage{
+    width: 100%;
+    height: 100vh;
+    font-family: 'Star Wars', sans-serif;
+    /* display: flex; */
+}
+@import url('https://fonts.cdnfonts.com/css/star-jedi');
+@import url('https://fonts.cdnfonts.com/css/star-wars');
+
+img{
+    width: 80px;
+}
+
+.btn{
+    background-color:rgba(18,220,246,255) !important;
+    border: rgba(18,220,246,255) !important;
+}
 
 </style>
